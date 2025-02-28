09f8d78d412ac8e0f92a0d47cbcf18d80f1ded40
Amin <113538948+Ayymin@users.noreply.github.com>
2025-02-28 03:10:42 +0100

    Update TaskController.java

--- ServerPrototype/src/main/java/ScheduleQuest/backendPrototype/ServerPrototype/Controller/TaskController.java
+++ ServerPrototype/src/main/java/ScheduleQuest/backendPrototype/ServerPrototype/Controller/TaskController.java
@@ -26,7 +26,7 @@ public class TaskController {
 
     @GetMapping
     public List<Task> getAllTasks() {
-        try (Connection connection = PostgreDB.connectToDB("postgres", "postgres", "Ern39sur")) {
+        try (Connection connection = PostgreDB.connectToDB("postgres", "postgres", "password")) {
             return taskCRUD.getAllTasks(connection);
         } catch (SQLException e) {
             throw new RuntimeException("Database error: " + e.getMessage(), e);
